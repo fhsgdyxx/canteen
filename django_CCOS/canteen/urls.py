cbc90fe657dc76fdcedbba4346f15f113f4d95a5
Wang Zixuan <3528124487@qq.com>
2025-06-01 14:52:38 +0800

    Add files via upload

---
+++ django_CCOS/canteen/urls.py
@@ -0,0 +1,11 @@
+from . import views
+from django.urls import re_path
+from django.urls import path
+from .views import show_canteen,show_shop
+
+
+app_name = 'canteen'
+urlpatterns = [
+    path('', show_canteen, name='canteen'),
+    path('shop/', show_shop, name='shop'),
+]
